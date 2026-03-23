dbf96162b8452b225abcb6096f4ad9100f60a966
aarchinakum5-Aarchi <aarchinakum5@gmail.com>
2026-03-23 10:22:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Rectangle
+{
+    private double length;
+    private double width;
+
+    
+    public Rectangle()
+    {
+        length = 0;
+        width = 0;
+    }
+
+    public Rectangle(double l, double w)
+    {
+        length = l;
+        width = w;
+    }
+
+    
+    public double GetArea()
+    {
+        return length * width;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Length: " + length);
+        Console.WriteLine("Width: " + width);
+        Console.WriteLine("Area: " + GetArea());
+    }
+}
