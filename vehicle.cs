f947f27e0ad789b75ea0233ff52775f520af4cb1
aarchinakum5-Aarchi <aarchinakum5@gmail.com>
2026-03-24 08:35:29 +0530

    Add files via upload

---
+++ vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit4_prog21
+{
+    internal class Vehicle
+    {
+        string vehicle_type;
+
+        public void get_data()
+        {
+            Console.WriteLine("enter vehicle type");
+            vehicle_type = Console.ReadLine();
+
+
+        }
+        public void show_vehicle()
+        {
+            Console.WriteLine("WriteLine type " + vehicle_type);
+        }
+
+    }
+}
